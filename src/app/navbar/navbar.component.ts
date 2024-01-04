7b4c9406c853a35e07f0e92e4f92072e232f68c7
221evans <jayevo95@gmail.com>
2024-01-04 18:28:32 +0000

    Integrate Angular localization and ng-bootstrap modules
    Added "@angular/localize" and "@ng-bootstrap/ng-bootstrap" to the project's dependencies in package.json. Also, imported and declared the new NavbarComponent in the main app component. Adjusted Angular compiler options to include "@angular/localize".

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkBase, NgbNavOutlet} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbNav,
+    NgbNavItem,
+    NgbNavLinkBase,
+    NgbNavContent,
+    NgbNavOutlet
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+
 
 }
